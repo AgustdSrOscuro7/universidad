823e3bd74972bff80717d3ef912a76dac586b383
AgustdSrOscuro7 <valekrudas1@gmail.com>
2024-08-29 20:32:05 -0500

    commit

--- Base/src/main/java/com/app/alumno/repository/AlumnoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.alumno.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.app.alumno.entity.Alumno;
-
-public interface AlumnoRepository extends CrudRepository<Alumno, Long> {
-
-}
--- formulario/src/main/java/com/app/formulario/repository/FormularioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.formulario.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.app.formulario.entity.Formulario;
-
-public interface FormularioRepository extends CrudRepository<Formulario, Long> {
-
-}
